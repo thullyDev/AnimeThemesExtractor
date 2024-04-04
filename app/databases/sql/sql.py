7d821f061063e57ba415036a5ae764a51d7680ec
thullyDev <thully.developer@gmail.com>
2024-04-04 07:25:16 +0200

    made the model global within the sql instance

--- app/databases/sql/sql.py
+++ app/databases/sql/sql.py
@@ -5,27 +5,27 @@ from . import get_models, get_model
 from .setup import engine
 
 class Sql:
+	def __init__(self, unit) -> None:
+		self.model = get_model(unit)
+
 	def sql_set(self, data: Dict[str, Any], *, unit: str) -> SQLModel:
-		model = get_model(unit)
 		with Session(engine) as session:
-			instance = model.model_validate(**data)
+			instance = self.model.model_validate(**data)
 			session.add(instance)
 			session.commit()
 			session.refresh(instance)
 			return instance
 
 	def sql_get(self, *, unit: str, **kwargs) -> Union[SQLModel, None]:
-		model = get_model(unit)
 		with Session(engine) as session:
-			instance = session.get(model, **kwargs)
+			instance = session.get(self.model, **kwargs)
 			if not instance:
 				return None
 			return instance
 
 	def sql_update(self, *, unit: str, data: Dict[str, Any], **kwargs) -> Union[SQLModel, None]:
-		model = get_model(unit)
 		with Session(engine) as session:
-			instance = session.get(model, **kwargs)
+			instance = session.get(self.model, **kwargs)
 
 			if not instance:
 				return None
@@ -37,9 +37,8 @@ class Sql:
 			return instance
 
 	def sql_delete(self, *, unit: str, **kwargs) -> bool:
-		model = get_model(unit)
 		with Session(engine) as session:
-			instance = session.get(model, **kwargs)
+			instance = session.get(self.model, **kwargs)
 
 			if not instance:
 				return False
@@ -49,13 +48,11 @@ class Sql:
 			return True
 
 	def sql_get_all(self, *, unit: str) -> Sequence:
-		model = get_model(unit)
 		with Session(engine) as session:
-			data = session.exec(select(model)).all()
+			data = session.exec(select(self.model)).all()
 			return data
 			
 	def sql_get_query(self, *, unit: str, **kwargs) -> Sequence:
-		model = get_model(unit)
 		with Session(engine) as session:
-			data = session.exec(select(model).filter(**kwargs)).all()
+			data = session.exec(select(self.model).filter(**kwargs)).all()
 			return data
