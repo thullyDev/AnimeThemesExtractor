5fe513115e0a4795bb33b140890f5a938e613488
thullyDev <thully.developer@gmail.com>
2024-04-04 19:15:32 +0200

    started the services instances

---
+++ app/databases/__init__.py
@@ -0,0 +1,2 @@
+from .sql import create_db_and_tables, Sql
+from .cache.cache import Cache
--- app/databases/sql/setup.py
+++ app/databases/sql/setup.py
@@ -9,3 +9,6 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_db_and_tables()
